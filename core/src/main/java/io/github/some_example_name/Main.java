aaffd9547bb7c3150b5e573ff628533f56a525a2
CaCayozin517 <cayoleal644@gmail.com>
2026-04-20 21:20:47 -0300

    inicial

---
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -0,0 +1,42 @@
+package io.github.some_example_name;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    private SpriteBatch batch;
+    private Texture image;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        image = new Texture("DiamondRush.png");
+    }
+
+    @Override
+    public void render() {
+        // Limpa a tela com preto para vermos as bordas
+        ScreenUtils.clear(0, 0, 0, 1f);
+
+        // Pegamos o tamanho atual da janela do jogo
+        int larguraJanela = com.badlogic.gdx.Gdx.graphics.getWidth();
+        int alturaJanela = com.badlogic.gdx.Gdx.graphics.getHeight();
+
+        batch.begin();
+
+        // O segredo está aqui: passamos 5 parâmetros para o draw
+        // image, x, y, largura, altura
+        batch.draw(image, 0, 0, larguraJanela, alturaJanela);
+
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        image.dispose();
+    }
+}
